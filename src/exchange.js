ad691d326a326754ac29f3807eb7e5fa23d5847a
dzhyhun <danilsavc@gmail.com>
2023-04-19 14:30:44 +0300

    Refactoring exchange.js

--- src/exchange.js
+++ src/exchange.js
@@ -1,5 +1,7 @@
-export const exchange = () => {
-  return fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&json")
+export const exchange = async () => {
+  return await fetch(
+    "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&json"
+  )
     .then((response) => response.json())
     .then((data) => data[0].rate);
 };
